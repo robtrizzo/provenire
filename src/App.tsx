9765c3271f302ab740459b3ef9c7efa043dde16b
Robert Rizzo <robtrizzo@gmail.com>
2023-03-31 23:58:58 -0400

    initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,87 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import baseAbilities from './data/abilities.json';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [abilities, setAbilities] = useState(baseAbilities);
+  const handleAbilityScoreChange = (
+    category: string,
+    ability: string,
+    newScore: number
+  ) => {
+    const newAbilities = abilities.map((cat) => {
+      if (cat.category === category) {
+        return {
+          ...cat,
+          abilities: cat.abilities.map((ab) => {
+            if (ab.name === ability) {
+              return {
+                ...ab,
+                score: newScore,
+              };
+            }
+            return ab;
+          }),
+        };
+      }
+      return cat;
+    });
+    setAbilities(newAbilities);
+  };
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="App-header">
+        <h1>PTBA + Disco Elysium = ???</h1>
+      </header>
+      <main>
+        <article className="abilities-article">
+          {abilities.map(({ category, abilities, score }) => (
+            <section key={`category-${category}`} className="abilities-row">
+              <div className={`ability-category ability-category-${category}`}>
+                <h3>{category}</h3>
+                <h4 className="category-score">{score}</h4>
+              </div>
+              {abilities.map(({ name, score }) => (
+                <div
+                  key={`ability-${name}`}
+                  className={`ability ability-category-${category}`}
+                >
+                  <h4 className="ability-name">{name}</h4>
+                  <div className="ability-score-section">
+                    <h5
+                      className="ability-score ability-score-button"
+                      onClick={() => {
+                        handleAbilityScoreChange(
+                          category,
+                          name,
+                          Math.max(score - 1, -2)
+                        );
+                      }}
+                    >
+                      -
+                    </h5>
+                    <h5 className="ability-score">{score}</h5>
+                    <h5
+                      className="ability-score ability-score-button"
+                      onClick={() => {
+                        handleAbilityScoreChange(
+                          category,
+                          name,
+                          Math.min(score + 1, 3)
+                        );
+                      }}
+                    >
+                      +
+                    </h5>
+                  </div>
+                </div>
+              ))}
+            </section>
+          ))}
+        </article>
+        <aside></aside>
+      </main>
+      <footer></footer>
     </div>
   );
 }
