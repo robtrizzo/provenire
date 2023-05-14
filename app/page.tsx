b41384d1f9510e1dcaffafd99e01dd4efce07954
Robert Rizzo <robtrizzo@gmail.com>
2023-05-14 18:59:05 -0400

    created smooth light/dark transition on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,30 @@ import { ThemeContext } from './theme-provider';
 export default function Home() {
   const { theme } = useContext(ThemeContext);
   return (
-    <div
-      className={`App flex w-full h-full flex-col transition-all duration-500 bg-gradient-to-bl ${
-        theme === 'light' ? 'from-gray-50 from-60%' : 'from-gray-900 from-40%'
-      }`}
-    >
-      <div className="flex w-full grow items-center justify-center">
-        <div className="relative w-80 h-80">
-          <img
-            src="https://provenire.s3.amazonaws.com/logo_2_alpha.png"
-            className={`absolute top-0 left-0 rounded-2xl transition-all duration-400  w-80 h-80 object-cover bg-inherit ${
-              theme === 'light' ? 'opacity-100' : 'opacity-0'
-            }`}
-          />
+    <div className={`App flex w-full h-full flex-col relative`}>
+      <div
+        className={`absolute top-0 left-0 flex h-full w-full grow items-center justify-center bg-gradient-to-bl from-gray-900 from-40% transition-all duration-1000 ${
+          theme === 'light' ? 'opacity-0' : 'opacity-100'
+        }`}
+      >
+        <div className="w-80 h-80">
           <img
             src="https://provenire.s3.amazonaws.com/logo_3_alpha.png"
             className={`bg-inherit top-0 left-0  rounded-2xl transition-all duration-400  w-80 h-80 object-cover ${
               theme === 'light' ? 'opacity-0' : 'opacity-100'
             }`}
+          />
+        </div>
+      </div>
+      <div
+        className={`absolute top-0 left-0 flex h-full w-full grow items-center justify-center bg-gradient-to-bl from-gray-50 from-60% transition-all duration-1000 ${
+          theme === 'light' ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
+        <div className="w-80 h-80">
+          <img
+            src="https://provenire.s3.amazonaws.com/logo_2_alpha.png"
+            className={`bg-inherit top-0 left-0  rounded-2xl transition-all duration-400  w-80 h-80 object-cover`}
           />
         </div>
       </div>
