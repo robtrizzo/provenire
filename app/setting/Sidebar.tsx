c216905bb21fc4239e571b011fdce1d8ef669104
Robert Rizzo <robtrizzo@gmail.com>
2023-06-02 14:43:37 -0400

    setting pages content

--- app/setting/Sidebar.tsx
+++ app/setting/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode, useContext } from "react";
+import React, { ReactNode, useContext, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ThemeContext } from "../theme-provider";
@@ -8,34 +8,55 @@ interface SidebarProps {
   children: ReactNode;
 }
 
-const navigation = [
-  { name: "World", href: "/setting" },
-  { name: "Arboria", href: "/setting/Linkrboria" },
+const navigation = [{ name: "World", href: "/setting" }];
+
+const era_one_navigation = [
+  { name: "World", href: "/setting/era_one" },
+  { name: "Arboria", href: "/setting/era_one/Linkrboria" },
   {
     name: "Cumeria",
-    href: "/setting/cumeria",
+    href: "/setting/era_one/cumeria",
   },
-  { name: "Gredora", href: "/setting/gredora" },
+  { name: "Gredora", href: "/setting/era_one/gredora" },
   {
     name: "Kilder",
-    href: "/setting/kilder",
+    href: "/setting/era_one/kilder",
   },
   {
     name: "Narscillia",
-    href: "/setting/narscillia",
+    href: "/setting/era_one/narscillia",
   },
   {
     name: "Rath",
-    href: "/setting/rath",
+    href: "/setting/era_one/rath",
   },
   {
     name: "Shian Tor",
-    href: "/setting/shianTor",
+    href: "/setting/era_one/shianTor",
   },
 ];
+
+const era_two_navigation = [
+  { name: "World", href: "/setting/era_two" },
+  { name: "Anidinie", href: "/setting/era_two/anidinie" },
+  { name: "Argos", href: "/setting/era_two/argos" },
+  { name: "Bwarhei", href: "/setting/era_two/bwarhei" },
+  { name: "Cumeria", href: "/setting/era_two/cumeria" },
+  { name: "Fenrir", href: "/setting/era_two/fenrir" },
+  { name: "Gredora", href: "/setting/era_two/gredora" },
+  { name: "Heia", href: "/setting/era_two/heia" },
+  { name: "Kilder", href: "/setting/era_two/kilder" },
+  { name: "Kipos", href: "/setting/era_two/kipos" },
+  { name: "Narscillia", href: "/setting/era_two/narscillia" },
+  { name: "Yama", href: "/setting/era_two/yama" },
+];
+
 export default function Sidebar({ children }: SidebarProps) {
   const pathname = usePathname();
   const { theme } = useContext(ThemeContext);
+
+  const [eraOneExpanded, setEraOneExpanded] = useState(true);
+  const [eraTwoExpanded, setEraTwoExpanded] = useState(false);
 
   return (
     <>
@@ -58,6 +79,126 @@ export default function Sidebar({ children }: SidebarProps) {
                 </Link>
               </li>
             ))}
+            <li>
+              <button
+                type="button"
+                className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                aria-controls="dropdown-example"
+                data-collapse-toggle="dropdown-example"
+                onClick={() => setEraOneExpanded(!eraOneExpanded)}
+              >
+                <span
+                  className="flex-1 ml-3 text-left whitespace-nowrap"
+                  sidebar-toggle-item
+                >
+                  Era One
+                </span>
+                {eraOneExpanded ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14.77 12.79a.75.75 0 01-1.06-.02L10 8.832 6.29 12.77a.75.75 0 11-1.08-1.04l4.25-4.5a.75.75 0 011.08 0l4.25 4.5a.75.75 0 01-.02 1.06z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+              </button>
+              <ul
+                id="dropdown-example"
+                className={`${eraOneExpanded ? "" : "hidden"} py-2 space-y-2`}
+              >
+                {era_one_navigation.map((item) => (
+                  <li className="pl-2">
+                    <Link
+                      href={item.href}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
+                        pathname === item.href ? "bg-gray-900" : ""
+                      }`}
+                    >
+                      <span className="ml-3">{item.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                aria-controls="dropdown-example"
+                data-collapse-toggle="dropdown-example"
+                onClick={() => setEraTwoExpanded(!eraTwoExpanded)}
+              >
+                <span
+                  className="flex-1 ml-3 text-left whitespace-nowrap"
+                  sidebar-toggle-item
+                >
+                  Era Two
+                </span>
+                {eraTwoExpanded ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14.77 12.79a.75.75 0 01-1.06-.02L10 8.832 6.29 12.77a.75.75 0 11-1.08-1.04l4.25-4.5a.75.75 0 011.08 0l4.25 4.5a.75.75 0 01-.02 1.06z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+              </button>
+              <ul
+                id="dropdown-example"
+                className={`${eraTwoExpanded ? "" : "hidden"} py-2 space-y-2`}
+              >
+                {era_two_navigation.map((item) => (
+                  <li className="pl-2">
+                    <Link
+                      href={item.href}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
+                        pathname === item.href ? "bg-gray-900" : ""
+                      }`}
+                    >
+                      <span className="ml-3">{item.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
           </ul>
         </div>
       </aside>
@@ -65,12 +206,19 @@ export default function Sidebar({ children }: SidebarProps) {
       <div
         className={`p-4 sm:ml-64 flex-grow h-full ${
           theme === "light"
-            ? "bg-gradient-to-bl from-orange-50 from-60% text-black"
+            ? "bg-gradient-to-tr from-orange-50 from-60% text-black"
             : "bg-gradient-to-bl from-stone-950 from-40% text-white"
         }`}
-        // className={`p-4 sm:ml-64 flex-grow h-full bg-parchment bg-cover bg-center bg-fixed bg-no-repeat bg-atta text-black`}
       >
-        {children}
+        <article
+          className={`p-2 prose lg:prose-xl prose-p:ml-4 prose-p:my-3 ${
+            theme === "light"
+              ? "prose-stone prose-h1:text-red-950 prose-h2:text-red-950"
+              : "prose-h1:text-red-900 prose-h2:text-red-900 prose-invert"
+          }`}
+        >
+          {children}
+        </article>
       </div>
     </>
   );
--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,3 +1,68 @@
 export default function World() {
-  return <div>test</div>;
+  return (
+    <>
+      <h1>Before anyone can remember</h1>
+      <p>
+        <strong>Jolana Arbor</strong> woke up.
+      </p>
+      <p>She wandered fields resting thousands of slumberers.</p>
+      <p>
+        Until she saw <strong>it</strong>. A bright monolith, suspended above
+        her kin.
+      </p>
+      <p>
+        She reached out to <strong>it</strong>, frightened, but awed by{" "}
+        <strong>it's</strong> beauty and warmth.
+      </p>
+      <p>
+        <strong>It</strong> filled her outstretched hand with{" "}
+        <strong>water</strong>.
+      </p>
+      <p>
+        <strong>Jolana</strong> had never known thirst, but a new instinct
+        compelled her to drink.
+      </p>
+      <p>
+        <strong>Jolana</strong> had never known danger, but new senses awoke her
+        to impending calamity.
+      </p>
+      <p>
+        <strong>Jolana</strong> had never known war, but new power she used to
+        hide away the slumberers.
+      </p>
+      <p>
+        <strong>She</strong> scattered them to the winds, trees, and sands in
+        the corners of the world.
+      </p>
+      <p>
+        They somehow survived the battle <strong>she</strong> fought for them.
+      </p>
+      <p>And eventually one woke up.</p>
+      <p>
+        Her first sight was of <strong>Jolana</strong>, who named her{" "}
+        <strong>Anya</strong>, and daughter.
+      </p>
+      <p>
+        Before <strong>Anya</strong> could speak a word to her mother,{" "}
+        <strong>Jolana</strong> was swallowed by the heavens, never to be seen
+        again.
+      </p>
+      <h2>Waking up</h2>
+      <p>
+        After <strong>Anya</strong>, many more people started stirring from
+        sleep, but they found themselves in a harsh and unforgiving world.
+      </p>
+      <p>
+        Each person boldly spoke a language that only they could understand.
+      </p>
+      <p>
+        Water was nowhere to be found, people instead subsisting off of animal
+        blood.
+      </p>
+      <p>
+        Strange monoliths dot the varied lanscapes of the world, gigantic and
+        enigmatic.
+      </p>
+    </>
+  );
 }
