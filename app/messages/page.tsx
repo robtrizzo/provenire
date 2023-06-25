a945251d7f2e6a72921358daaa6ac5df91e1db3c
Robert Rizzo <robtrizzo@gmail.com>
2023-06-25 18:43:25 -0400

    button component, utils for variants and configurable overwritable tailwind classnames

---
+++ app/messages/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/Button';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button className="w-20">hello</Button>;
+};
+
+export default page;
