f064b3e9faf9c483dadf5d30e34274cd526d413f
Robert Rizzo <robtrizzo@gmail.com>
2023-10-16 18:44:23 -0400

    osg bg lightmode

--- app/osg/twisted_scion/layout.tsx
+++ app/osg/twisted_scion/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <div
       className={`p-8 max-w-none prose prose-lead:mt-0 lg:prose-xl prose-p:ml-4 prose-p:my-1 prose-p:font-serif ${
         theme === 'light'
-          ? 'prose-stone prose-h1:text-red-950 prose-h2:text-red-950 prose-h3:text-red-950 prose-h4:text-red-950'
+          ? 'prose-stone bg-slate-50 prose-h1:text-red-950 prose-h2:text-red-950 prose-h3:text-red-950 prose-h4:text-red-950'
           : 'prose-h1:text-indigo-400 prose-h2:text-indigo-400 prose-invert prose-h3:text-indigo-400 prose-h4:text-indigo-400'
       }`}
     >
