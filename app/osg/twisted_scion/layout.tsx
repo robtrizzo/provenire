218843dc22f7a3ea4b60818216a89beef91db59c
Robert Rizzo <robtrizzo@gmail.com>
2023-10-30 22:56:08 -0400

    explicit black background for twisted scion darkmode

--- app/osg/twisted_scion/layout.tsx
+++ app/osg/twisted_scion/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       className={`p-8 max-w-none prose prose-lead:mt-0 lg:prose-xl prose-p:ml-4 prose-p:my-1 prose-p:font-serif ${
         theme === 'light'
           ? 'prose-stone bg-slate-50 prose-h1:text-red-950 prose-h2:text-red-950 prose-h3:text-red-950 prose-h4:text-red-950'
-          : 'prose-h1:text-indigo-400 prose-h2:text-indigo-400 prose-invert prose-h3:text-indigo-400 prose-h4:text-indigo-400'
+          : 'prose-h1:text-indigo-400 bg-black prose-h2:text-indigo-400 prose-invert prose-h3:text-indigo-400 prose-h4:text-indigo-400'
       }`}
     >
       {children}
