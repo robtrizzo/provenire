697402cb4628bbc9e712938da99b89997564f090
Robert Rizzo <robtrizzo@gmail.com>
2024-06-12 21:58:58 -0400

    added page transitions with framer-motion

---
+++ components/animations/transition.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+export default function Transition({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: 'easeInOut', duration: 0.33 }}
+      className="flex flex-col gap-4"
+    >
+      {children}
+    </motion.div>
+  );
+}
--- components/blocks/sidebar.tsx
+++ components/blocks/sidebar.tsx
@@ -564,9 +564,7 @@ export function Sidebar({ children }: { children: React.ReactNode }) {
             <ModeToggle />
           </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          {children}
-        </main>
+        <main className="flex-1 p-4 lg:gap-6 lg:p-6">{children}</main>
       </div>
     </div>
   );
