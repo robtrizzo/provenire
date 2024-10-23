c07cf57317b0bb0d06cdd51b2f2f7f90b51e7bd1
Robert Rizzo <robtrizzo@gmail.com>
2024-10-23 19:43:22 -0400

    adjustments to playtest one page

--- app/game/playtest-one/page.tsx
+++ app/game/playtest-one/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { Separator } from '@/components/ui/separator';
 import {
-  TypographyBlockquote,
   TypographyH1,
   TypographyH3,
   TypographyP,
@@ -16,51 +15,12 @@ export default async function Page() {
         The playtest will involve character creation and two full cycles of{' '}
         <span className="italic">prelude</span> -{'>'}{' '}
         <span className="italic">mission</span> -{'>'}{' '}
-        <span className="italic">churn</span>. My primary goal for playtest one
-        is to evaluate the core rules.
+        <span className="italic">churn</span>. You&apos;ll be playing a
+        character that you won&apos;t be playing in the main game. But the
+        events of this playtest <i>will</i> be canonical to the story of the main game.
+        So please experiment and push the boundaries of the game, but remember
+        that your choices will linger.
       </TypographyP>
-      <TypographyUnorderedList>
-        <li>Do they work?</li>
-        <li>Can I explain them clearly?</li>
-        <li>Are they fun?</li>
-        <li>
-          Do they cover the types of situations you want to explore in the game?
-        </li>
-      </TypographyUnorderedList>
-      <TypographyP>
-        Next, I want feedback on the{' '}
-        <span className="text-sky-500">heritage</span> and{' '}
-        <span className="text-amber-700">archetype</span> options. Specifically:
-      </TypographyP>
-      <TypographyUnorderedList>
-        <li>Are they compelling?</li>
-        <li>Are there too many or not enough?</li>
-        <li>Do any seem obviously under or overpowered?</li>
-      </TypographyUnorderedList>
-      <TypographyP>
-        Lastly, I want to get a gague on setting and tone.
-      </TypographyP>
-      <TypographyUnorderedList>
-        <li>Is it compelling?</li>
-        <li>
-          Can I convey a dark setting while maintaining an optimistic overtone?
-        </li>
-        <li>
-          Are there any parts which the players are wholly uninterested in or
-          want to be expanded?
-        </li>
-      </TypographyUnorderedList>
-      <TypographyP>
-        These goals aren&apos;t gospel and I&apos;m primarily focused on running
-        a fun session for everyone. But if you could keep these questions in
-        mind as we play and give me feedback at the end, I&apos;d appreciate
-        that very much!
-      </TypographyP>
-      <TypographyBlockquote>
-        The events of this playtest will be canonical to the story of the main
-        game. So please experiment and push the boundaries of the game, but
-        remember that your choices will linger.
-      </TypographyBlockquote>
       <Separator className="my-5" />
       <TypographyH3>Primer</TypographyH3>
       <TypographyP>
@@ -81,7 +41,7 @@ export default async function Page() {
         in the back of cold storage every few days. Whatever it is,
         &apos;something incredible&apos; sounds nice.
       </TypographyP>
-      <Separator />
+      <Separator className="my-5" />
       <TypographyH3>Character Creation</TypographyH3>
       <TypographyP>
         We will be going through a limited version of character creation for
@@ -125,6 +85,53 @@ export default async function Page() {
         To wrap up, name your <strong>bonds</strong> and assign 6 points to your
         actions. <span className="text-red-500">Red dots</span> cost 1 point and{' '}
         <span className="text-blue-500">blue dots</span> cost 2 points.
+      </TypographyP>
+      <Separator className="my-5" />
+      <TypographyH3>Feedback</TypographyH3>
+      <TypographyP>
+        Once session is over, don&apos;t feel the need to stick around any
+        longer, but that&apos;s when I&apos;ll be seeking your feedback. If you
+        do stay, thank you! I appreciate it.
+      </TypographyP>
+      <TypographyP>
+        My primary goal for playtest one is to evaluate the core rules.
+      </TypographyP>
+      <TypographyUnorderedList>
+        <li>Do they work?</li>
+        <li>Can I explain them clearly?</li>
+        <li>Are they fun?</li>
+        <li>
+          Do they cover the types of situations you want to explore in the game?
+        </li>
+      </TypographyUnorderedList>
+      <TypographyP>
+        Next, I want feedback on the{' '}
+        <span className="text-sky-500">heritage</span> and{' '}
+        <span className="text-amber-700">archetype</span> options. Specifically:
+      </TypographyP>
+      <TypographyUnorderedList>
+        <li>Are they compelling?</li>
+        <li>Are there too many or not enough?</li>
+        <li>Do any seem obviously under or overpowered?</li>
+      </TypographyUnorderedList>
+      <TypographyP>
+        Lastly, I want to get a gague on setting and tone.
+      </TypographyP>
+      <TypographyUnorderedList>
+        <li>Is it compelling?</li>
+        <li>
+          Can I convey a dark setting while maintaining an optimistic overtone?
+        </li>
+        <li>
+          Are there any parts which the players are wholly uninterested in or
+          want to be expanded?
+        </li>
+      </TypographyUnorderedList>
+      <TypographyP>
+        These goals aren&apos;t gospel and I&apos;m primarily focused on running
+        a fun session for everyone. But if you could keep these questions in
+        mind as we play and give me feedback at the end, I&apos;d appreciate
+        that very much!
       </TypographyP>
     </>
   );
