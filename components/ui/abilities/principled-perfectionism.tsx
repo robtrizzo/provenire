4e4d97b74435a470a6e977ad2a4e952cb2321528
Robert Rizzo <robtrizzo@gmail.com>
2024-11-14 15:09:22 -0500

    fix typo

--- components/ui/abilities/principled-perfectionism.tsx
+++ components/ui/abilities/principled-perfectionism.tsx
@@ -3,8 +3,8 @@ export default function PrincipledPerfectionism() {
   return (
     <div>
       <TypographyP>
-        Visionaries get an extra downtime activity if they have spent an action
-        to help someone else. If they haven&apos;t, they take <b>-2 ticks</b> on
+        Advocates get an extra downtime activity if they have spent an action to
+        help someone else. If they haven&apos;t, they take <b>-2 ticks</b> on
         their <b>recovery clock</b>.
       </TypographyP>
     </div>
