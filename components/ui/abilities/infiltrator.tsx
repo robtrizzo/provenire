a48dcf8667c50bf174d0331a1e9b5f659c2cbfa8
Robert Rizzo <robtrizzo@gmail.com>
2024-11-16 19:12:19 -0500

    fix typo

--- components/ui/abilities/infiltrator.tsx
+++ components/ui/abilities/infiltrator.tsx
@@ -2,7 +2,7 @@ import { TypographyP } from '@/components/ui/typography';
 export default function Infiltrator() {
   return (
     <TypographyP>
-      You are not capable of bypassing all manner of security measures.
+      You are capable of bypassing all manner of security measures.
     </TypographyP>
   );
 }
