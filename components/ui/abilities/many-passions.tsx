e965638fdb08794ece5afd70f435b6c993a6c7f8
Robert Rizzo <robtrizzo@gmail.com>
2024-11-21 14:34:26 -0500

    artist updated

--- components/ui/abilities/many-passions.tsx
+++ components/ui/abilities/many-passions.tsx
@@ -3,13 +3,8 @@ export default function ManyPassions() {
   return (
     <div>
       <TypographyP>
-        For each attribute (<i>Heart, Instinct, Machina</i>), the Artist gains{' '}
-        <strong>+1d</strong> on the first roll they make with an action in
-        attribute&apos;s category each mission. On the third or later rolls they
-        make with actions in a particular attribute, they take{' '}
-        <strong>-1d</strong>. At the end of the mission, if they&apos;ve made
-        four or more rolls with unique action combinations, they mark{' '}
-        <strong>1 xp</strong>.
+        Gain an additional xp trigger:{' '}
+        <i>Did you use 4 or more unique action combinations?</i>
       </TypographyP>
     </div>
   );
