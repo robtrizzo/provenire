0daea7a57621fb6ebc73b09205057cbe4ce71155
Robert Rizzo <robtrizzo@gmail.com>
2024-11-21 11:28:55 -0500

    update to not redirect on socket.io calls

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,8 @@ const allowedBetaUsers = ['robtrizzo@gmail.com', 'robtrizzo+jerry@gmail.com'];
 
 export default auth((req) => {
   const reqUrl = new URL(req.url);
-  if (!req.auth && reqUrl.pathname !== '/') {
+  if (!req.auth && !['/', '/signin', '/socket.io'].includes(reqUrl.pathname)) {
+    console.log('pathname', reqUrl.pathname);
     return NextResponse.redirect(
       new URL(
         `${BASE_PATH}/signin?callbackUrl=${encodeURIComponent(
