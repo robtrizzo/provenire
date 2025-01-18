97e1d8052c190653f64b2689a9141b0137038f06
Robert Rizzo <robtrizzo@gmail.com>
2025-01-18 16:14:42 -0500

    temporarily disable eslint for this file. will reenable once db refactor is complete

--- app/game/character-sheet/(components)/load-character.tsx
+++ app/game/character-sheet/(components)/load-character.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
