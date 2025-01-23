b858e059dc9f9733271cde7195afc27706615de7
Robert Rizzo <robtrizzo@gmail.com>
2025-01-23 18:53:25 -0500

    fix /users/userid: PUT args

--- app/api/users/[userid]/route.tsx
+++ app/api/users/[userid]/route.tsx
@@ -1,12 +1,15 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth/index";
 import { checkUserAuthenticated, checkUserRole } from "@/lib/auth";
 
-export async function PUT({
-  params,
-}: {
-  params: { userid: string };
-}): Promise<NextResponse> {
+export async function PUT(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: { userid: string };
+  }
+): Promise<NextResponse> {
   const session = await auth();
 
   const unauthenticatedResponse = checkUserAuthenticated(session);
