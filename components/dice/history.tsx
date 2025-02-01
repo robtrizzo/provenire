73ff51baec1ea16406dfdd8ab99e0b1c0d20e02b
David Kindel <dkindel@gmail.com>
2025-02-01 15:16:23 -0500

    Move dice files to a common component folder

--- components/dice/history.tsx
+++ components/dice/history.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
-import { DieDetails } from "./die-details";
-import DieAccordian from "./die-accordian";
+import { DieDetails } from "./details";
+import DieAccordian from "./accordian";
 import { Roll } from "@/types/roll";
 
 export default function DiceHistory({
