5772ef8a37270737fd340096dc8983ce9de94db8
Robert Rizzo <robtrizzo@gmail.com>
2025-02-07 14:39:14 -0500

    allow images from s3 bucket

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,37 @@
-import type { NextConfig } from 'next';
-import createMDX from '@next/mdx';
+import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    reactCompiler: true
+    reactCompiler: true,
   },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'mz3l6y8ywafu109t.public.blob.vercel-storage.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "mz3l6y8ywafu109t.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'provenire.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "provenire.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "provenire.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
