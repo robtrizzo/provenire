0fc416dbaac3bc5bf113180e946b01c6896e2c1c
Robert Rizzo <robtrizzo@gmail.com>
2025-04-16 10:46:47 -0400

    unlimited xp clocks

--- app/game/play/(components)/character-sheet/components/xp-clocks.tsx
+++ app/game/play/(components)/character-sheet/components/xp-clocks.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Clock from "@/components/clock";
 import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 export default function XPClocks({
   current,
   setVal,
@@ -9,26 +10,68 @@ export default function XPClocks({
   current: number;
   setVal: (n: number) => void;
 }) {
+  const { theme } = useTheme();
   const [xp, setXp] = useState<number>(current);
 
-  const numClocks = 5;
+  const dark = theme === "dark";
+
+  const borderThickness = 2;
+
+  const adjustedWidth = 35 - 2 * borderThickness;
+  const adjustedHeight = 35 - 2 * borderThickness;
+
+  const cx = adjustedWidth / 2;
+  const cy = adjustedHeight / 2;
+  const outerRadius = Math.min(adjustedWidth, adjustedHeight) / 2;
+
+  const spokes = Array.from({ length: 6 }, (_, i) => {
+    const angle = (360 / 6) * i - 90;
+    const x = cx + outerRadius * Math.cos((angle * Math.PI) / 180);
+    const y = cy + outerRadius * Math.sin((angle * Math.PI) / 180);
+    return (
+      <line
+        key={i}
+        x1={cx}
+        y1={cy}
+        x2={x}
+        y2={y}
+        stroke={dark ? "hsl(215 20.2% 65.1%)" : "hsl(215.4 16.3% 46.9%)"}
+        strokeWidth={1}
+      />
+    );
+  });
 
   return (
     <div className="p-1 flex items-center gap-2 justify-between select-none flex-wrap">
       <div className="p-1 flex items-center gap-2 select-none">
-        {Array.from({ length: numClocks }, (_, i) => {
-          const clockVal = Math.min(Math.max(xp - i * 6, 0), 6);
-          return (
-            <Clock
-              key={i}
-              width={35}
-              height={35}
-              max={6}
-              current={clockVal}
-              clickable={false}
-            />
-          );
-        })}
+        <Clock
+          width={35}
+          height={35}
+          max={6}
+          current={xp % 6}
+          clickable={false}
+        />
+        <div className="relative h-[35px] w-[35px] border-solid border-2 border-muted-foreground rounded-full bg-red-500">
+          <div className="absolute h-[35px] w-[35px] flex items-center justify-center">
+            <span
+              className="font-bold z-10 text-black text-2xl mr-0.5"
+              style={{
+                textShadow:
+                  "0px 0px 2px white, 0px 0px 2px white, 0px 0px 2px white",
+              }}
+            >
+              {Math.floor(xp / 6)}
+            </span>
+          </div>
+          <svg
+            width={35}
+            height={35}
+            viewBox={`0 0 ${35} ${35}`}
+            className="absolute z-0"
+          >
+            <g>{spokes}</g>
+          </svg>
+        </div>
       </div>
       <div className="flex">
         <Button
@@ -47,12 +90,9 @@ export default function XPClocks({
         <Button
           variant="ghost"
           className="text-xs text-muted-foreground text-center w-full"
-          disabled={xp >= 30}
           onClick={() => {
-            if (xp < 30) {
-              setXp(xp + 1);
-              setVal(xp + 1);
-            }
+            setXp(xp + 1);
+            setVal(xp + 1);
           }}
         >
           +xp
