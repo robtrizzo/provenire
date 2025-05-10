8cd2a5a015e666e2f2f3542ce9fdd12805e5866b
Robert Rizzo <robtrizzo@gmail.com>
2025-05-10 15:37:08 -0400

    remove bad export

--- app/api/characters/route.ts
+++ app/api/characters/route.ts
@@ -1,25 +1,13 @@
 import { NextResponse } from "next/server";
 import { checkAuth } from "@/lib/auth";
 import redis, { findKeysByPattern } from "@/lib/redis";
-import { Character } from "@/types/game";
+import { getAllCharacters } from "@/handlers/characters";
 
 async function getAllCharactersForUser(userId: string | undefined) {
   if (!userId) return [];
   const pattern = `user:${userId}:character:*`;
   const keys = await findKeysByPattern(pattern);
   const characters = await Promise.all(keys.map((key) => redis.get(key)));
-  return characters;
-}
-
-export async function getAllCharacters() {
-  const pattern = `user:*:character:*`;
-  const keys = await findKeysByPattern(pattern);
-  const characters = await Promise.all(
-    keys.map(async (key) => {
-      const character = (await redis.get(key)) as Character;
-      return { ...character, key };
-    })
-  );
   return characters;
 }
 
