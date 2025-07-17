7ade4b1ee109de19e9a451e24b4f1fbf1ba298ed
Robert Rizzo <robtrizzo@gmail.com>
2025-07-17 11:44:01 -0400

    operative pages

--- app/game/arc-two/character-creation/operatives/(components)/operative-card.tsx
+++ app/game/arc-two/character-creation/operatives/(components)/operative-card.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@/components/ui/card";
 import { TypographyH3 } from "@/components/ui/typography";
 import OpPortrait from "./operative-portrait";
+import Link from "next/link";
 export default function OperativeCard({
   name,
   children,
@@ -9,19 +10,21 @@ export default function OperativeCard({
   children?: React.ReactNode;
 }) {
   return (
-    <Card className="py-0 pr-2 px-2 lg:px-0 pb-2 lg:pb-0">
-      <div className="flex flex-col lg:flex-row gap-2">
-        <OpPortrait
-          width={200}
-          height={200}
-          name={name}
-          className="lg:rounded-l-lg mx-auto lg:mx-0"
-        />
-        <div>
-          <TypographyH3 className="font-cyber">{name}</TypographyH3>
-          {children}
+    <Link href={"operatives/" + name.toLocaleLowerCase()} className="group">
+      <Card className="py-0 pr-2 px-2 lg:px-0 pb-2 lg:pb-0 group-hover:border-fuchsia-700">
+        <div className="flex flex-col lg:flex-row gap-2">
+          <OpPortrait
+            width={200}
+            height={200}
+            name={name}
+            className="lg:rounded-l-lg mx-auto lg:mx-0"
+          />
+          <div>
+            <TypographyH3 className="font-cyber">{name}</TypographyH3>
+            {children}
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 }
