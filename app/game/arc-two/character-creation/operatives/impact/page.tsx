fcf1ff4d7b54a497391d399b7b58ebfd6ebab364
Robert Rizzo <robtrizzo@gmail.com>
2025-07-17 11:44:01 -0400

    operative pages

---
+++ app/game/arc-two/character-creation/operatives/impact/page.tsx
@@ -0,0 +1,25 @@
+import Breadcrumbs from "@/components/ui/breadcrumbs";
+import { TypographyH1 } from "@/components/ui/typography";
+
+export default async function Page() {
+  return (
+    <>
+      <Breadcrumbs
+        crumbs={[
+          { name: "Game", href: "/game" },
+          { name: "Arc Two", href: "/game/arc-two" },
+          {
+            name: "Character Creation",
+            href: "/game/arc-two/character-creation",
+          },
+          {
+            name: "Operatives",
+            href: "/game/arc-two/character-creation/operatives",
+          },
+          { name: "Impact", href: "#" },
+        ]}
+      />
+      <TypographyH1>Impact</TypographyH1>
+    </>
+  );
+}
