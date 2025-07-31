a2ab89fc38fadfb34534bdac335872fb0461fe3b
Robert Rizzo <robtrizzo@gmail.com>
2025-07-31 14:26:00 -0400

    Watchtower

--- app/game/arc-two/character-options/operatives/watchtower/page.tsx
+++ app/game/arc-two/character-options/operatives/watchtower/page.tsx
@@ -64,11 +64,13 @@ export default async function Page() {
         During missions Root may ask you to...
       </TypographyP>
       <TypographyUnorderedList>
-        <li>Run into a burning building</li>
-        <li>Jump in the way of traffic to save a teammate</li>
-        <li>Escape ten assassins with your hands tied behind your back</li>
-        <li>Diffuse a bomb</li>
-        <li>Start a fight when the talking gets too boring</li>
+        <li>Relay important events to mission control</li>
+        <li>Be the on-site director and frame the action</li>
+        <li>
+          Make sure good &quot;stunt partners&quot; don&apos;t die too soon
+        </li>
+        <li>Talk a hot-headed teammate down from going off-script</li>
+        <li>Spot new threats before they can impact the scene</li>
       </TypographyUnorderedList>
       <TypographyH2 className="font-cyber">
         Advanced Surveilance Swarm &quot;Watchdogs&quot;
@@ -78,32 +80,131 @@ export default async function Page() {
         <span>|</span>
         <span className="text-red-500">Crystech, Electrotech, Nettech</span>
       </div>
+      <TypographyP>
+        Top of the line tech born from an experimental collaboration between{" "}
+        <span className="font-cyber">Sentry</span> and{" "}
+        <span className="font-cyber">Cytech</span>. What were once
+        seek-and-destroy drones have been retrofitted with the latest and
+        greatest 3D camera technology. A short history of these drones gone
+        haywire has kept them off the commercial market for the time being.
+      </TypographyP>
+      <TypographyP>
+        <span className="font-cyber">Root</span> has even considered
+        decommisioning the{" "}
+        <span className="font-cyber">&quot;Watchdogs&quot;</span> project but{" "}
+        <b>Felicity</b>, <span className="font-cyber">Root&apos;s</span> media
+        coordinator, has insisted that the footage they get is just{" "}
+        <i>too good</i>. Besides. <span className="font-cyber">Watchtower</span>{" "}
+        and the <span className="font-cyber">&quot;Watchdogs&quot;</span>{" "}
+        project are tied at the hip. So it&apos;s on you to make them look good.
+        And don&apos;t say shit if they act up.
+      </TypographyP>
       <div className="mt-2 flex items-center gap-4">
         <TypographyH3 className="font-cyber mt-0">Saw it Coming</TypographyH3>
         <ClockCost num={0} />
       </div>
-      <div className="mt-2 flex items-center gap-4">
-        <TypographyH3 className="font-cyber mt-0">
-          Mission Control, Tell Corporate They Can Shove It.
-        </TypographyH3>
-        <ClockCost num={0} />
-      </div>
+      <TypographyP>
+        The <span className="font-cyber">&quot;Watchdogs&quot;</span> are a
+        potent surveilance tool, but it can also be incredibly overwhelming to
+        the senses. The neural implant that enables piloting the drones also
+        streams all of their data back to your brain and reconstructs a real
+        time 3D image of the surrounding area. This makes{" "}
+        <span className="font-cyber">Watchtower</span> incredibly capable of
+        spotting incoming threats.
+      </TypographyP>
+      <TypographyP>
+        When the team would be ambushed or surprised by a new threat,{" "}
+        <b>push yourself</b> to have already seen it coming. It&apos;s up to you
+        if you shared the intel with the team. Your call. Whatever creates the
+        most interesting combats.
+      </TypographyP>
       <div className="mt-2 flex items-center gap-4">
         <TypographyH3 className="font-cyber mt-0">
           Give Me the Fucking Camera
         </TypographyH3>
-        <ClockCost num={0} />
+        <ClockCost num={2} />
       </div>
+      <TypographyP>
+        <b>Felicity</b> and her team of media coordinators are in the
+        team&apos;s ear to make sure <span className="font-cyber">Cytech</span>{" "}
+        captures all the best footage. But they&apos;ve never been in your
+        shoes. You see the action first-hand and you know for a fact that
+        you&apos;re a better director than they are a lot of the time.
+      </TypographyP>
+      <TypographyP>
+        When you disagree with <b>Felicity</b> on a directing call,{" "}
+        <b>push yourself</b> and make an <b>action roll</b>. <b>1-5</b>: if it
+        goes wrong, it&apos;s on you, <b>mark 2 stress</b>; <b>4-6</b>: you can
+        reframe the action scene in the way you want; <b>Crit</b>: it&apos;s
+        fucking fire, both you and the filmed operative get a bonus 1 ¤P.
+      </TypographyP>
       <div className="mt-2 flex items-center gap-4">
         <TypographyH3 className="font-cyber mt-0">Attack Dogs</TypographyH3>
-        <ClockCost num={0} />
+        <ClockCost num={3} />
       </div>
+      <TypographyP>
+        Turns out the <span className="font-cyber">&quot;Watchdogs&quot;</span>{" "}
+        had less of their starting tech stripped out than you thought. It&apos;s
+        disabled, but nothing a skilled{" "}
+        <span className="font-cyber">Watchtower</span> can&apos;t undo. Once
+        reactivated, attacking with them is as easy as mentally indicating a
+        target. They take care of the rest.
+      </TypographyP>
+      <TypographyP>
+        Keep in mind that <b>Felicity</b> won&apos;t approve. So be subtle. And
+        for fuck&apos;s sake, don&apos;t get all the drones taken out in some
+        frivolous combat.
+      </TypographyP>
+      <div className="mt-2 flex items-center gap-4">
+        <TypographyH3 className="font-cyber mt-0">
+          Mission Control, Tell Corporate They Can Shove It.
+        </TypographyH3>
+        <ClockCost num={3} />
+      </div>
+      <span className="font-cyber text-muted-foreground">
+        <b>
+          <u>Prerequisite</u>:
+        </b>{" "}
+        Give Me the Fucking Camera
+      </span>
+      <TypographyP>
+        Sometimes <b>Felicity</b> forgets that this isn&apos;t a movie studio
+        and the guys your team is fighting - they&apos;re not stuntmen. And on
+        top of it all, you have a mission to accomplish that isn&apos;t just
+        getting cool shots goddamnit. Gentle reminders don&apos;t usually get
+        results, so experienced <span className="font-cyber">Watchtowers</span>{" "}
+        learn how to wield their gravitas.
+      </TypographyP>
+      <TypographyP>
+        When you disregard <span className="font-cyber">Cytech&apos;s</span>{" "}
+        motion capture objectives in favor of{" "}
+        <span className="font-cyber">Root&apos;s</span> counter-terrorist
+        mission, make an <b>action roll</b>. <b>1-3</b>: you work for <i>us</i>,
+        you can forget about any bonuses at the end of the mission; <b>4,5</b>:
+        fine, but you&apos;d better get some kind of footage or there&apos;ll be
+        hell to pay; <b>6</b>: heard, telling them to put it where it don&apos;t
+        shine; <b>Crit</b>: well done for your loyalty to <b>Kingwulf</b>, +1
+        ¤F.
+      </TypographyP>
       <div className="mt-2 flex items-center gap-4">
         <TypographyH3 className="font-cyber mt-0">
           Contract Escalation
         </TypographyH3>
-        <ClockCost num={0} />
+        <ClockCost num={5} />
       </div>
+      <span className="font-cyber text-muted-foreground">
+        <b>
+          <u>Prerequisite</u>:
+        </b>{" "}
+        Mission Control, Tell Corporate They Can Shove It.
+      </span>
+      <TypographyP>
+        You&apos;ve made yourself a valuable part of the{" "}
+        <span className="font-cyber">Cyctech / Root</span> back and forth. Both
+        orgs are always in need of talent like you and so its time your
+        responsibilities were increased. You&apos;ll be briefed on the mission
+        as a whole. The real mission.
+      </TypographyP>
     </>
   );
 }
