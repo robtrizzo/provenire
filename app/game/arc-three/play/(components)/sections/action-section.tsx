725b906313d41fce7494680f3ef7e6f159ab31ff
Robert Rizzo <robtrizzo@gmail.com>
2026-01-26 13:30:17 -0500

    small rendering improvements in actions section

--- app/game/arc-three/play/(components)/sections/action-section.tsx
+++ app/game/arc-three/play/(components)/sections/action-section.tsx
@@ -5,14 +5,14 @@ import Action from "../action";
 export default function ActionSection() {
   const { abilities, skills, MAX_ABILITIES, MAX_SKILLS } = useCharacterSheet();
   return (
-    <div className="mt-4 grid grid-cols-2 gap-1">
-      <TypographyH2 className="text-md mt-0 uppercase text-muted-foreground">
+    <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-1">
+      <TypographyH2 className="text-md mt-0 uppercase text-muted-foreground order-1">
         Abilities
       </TypographyH2>
-      <TypographyH2 className="text-md mt-0 uppercase text-muted-foreground">
+      <TypographyH2 className="text-md mt-0 uppercase text-muted-foreground order-2 md:order-1">
         Skills
       </TypographyH2>
-      <div className="col-span-1 flex flex-col gap-0.5">
+      <div className="col-span-1 flex flex-col gap-0.5 order-1">
         {abilities.map((a, idx) => (
           <Action.Wrapper.Tooltip action={a} key={idx + a.name}>
             <Action.Wrapper.Menu action={a}>
@@ -24,13 +24,15 @@ export default function ActionSection() {
         ))}
         {Array.from({ length: MAX_ABILITIES - abilities.length }).map(
           (_, idx) => (
-            <Action.Wrapper.Grid className="p-0" key={`unlock-ability-${idx}`}>
-              <Action.HeaderContent.Unlock type="ability" className="p-2" />
-            </Action.Wrapper.Grid>
+            <Action.HeaderContent.Unlock
+              type="ability"
+              className="p-2"
+              key={`unlock-ability-${idx}`}
+            />
           ),
         )}
       </div>
-      <div className="col-span-1 flex flex-col gap-0.5">
+      <div className="col-span-1 flex flex-col gap-0.5 order-3 md:order-1">
         {skills.map((a, idx) => (
           <Action.Wrapper.Tooltip action={a} key={idx + a.name}>
             <Action.Wrapper.Menu action={a}>
@@ -41,9 +43,11 @@ export default function ActionSection() {
           </Action.Wrapper.Tooltip>
         ))}
         {Array.from({ length: MAX_SKILLS - skills.length }).map((_, idx) => (
-          <Action.Wrapper.Grid className="p-0" key={`unlock-skill-${idx}`}>
-            <Action.HeaderContent.Unlock type="skill" className="p-2" />
-          </Action.Wrapper.Grid>
+          <Action.HeaderContent.Unlock
+            type="skill"
+            className="p-2"
+            key={`unlock-skill-${idx}`}
+          />
         ))}
       </div>
     </div>
