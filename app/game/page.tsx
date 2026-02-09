e0074094bae9f3e1c7e96fe415d772a873fdca37
Robert Rizzo <robtrizzo@gmail.com>
2026-02-09 15:57:12 -0500

    remembrances

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,3 +1,4 @@
+import { ThetaTriple } from "@/components/dice/dice-symbols";
 import { BorderBeam } from "@/components/magicui/border-beam";
 import { Card } from "@/components/ui/card";
 import { TypographyH1 } from "@/components/ui/typography";
@@ -26,6 +27,27 @@ export default async function Page() {
             Arc Two: Root
           </TypographyH1>
           <BorderBeam duration={8} size={100} />
+        </Card>
+      </Link>
+      <Link href="/game/arc-three">
+        <Card className="max-w-2xl py-18 mb-12 flex flex-col items-center justify-center mx-auto relative">
+          <TypographyH1 className="text-2xl font-bold m-0 flex items-center">
+            <span>Arc Three:</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="4 4 16 16"
+              width={100}
+              height={100}
+              fill="white"
+              stroke="white"
+              strokeWidth={0.8}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="text-white"
+            >
+              <ThetaTriple />
+            </svg>
+          </TypographyH1>
         </Card>
       </Link>
     </div>
