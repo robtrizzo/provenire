7d5d3d30ca5cb93f17827232e7e4393827c21ae6
Robert Rizzo <robtrizzo@gmail.com>
2026-03-06 00:38:56 -0500

    small tweak

--- components/abilities/skillsets/arc3/hammer/tough-as-nails.tsx
+++ components/abilities/skillsets/arc3/hammer/tough-as-nails.tsx
@@ -1,9 +1,10 @@
-import { TypographyP } from '@/components/ui/typography';
+import { TypographyP } from "@/components/ui/typography";
 export default function ToughAsNails() {
   return (
     <TypographyP>
       You gain a second space to record <b>level 3 harm</b>. When you suffer a
-      mortal wound, you may mark <b>3 stress</b> to stabilize it and stay alive.
+      mortal wound, you may mark <b>4 stress</b> to stabilize it. When combat
+      ends, you require immediate medical attention.
     </TypographyP>
   );
 }
