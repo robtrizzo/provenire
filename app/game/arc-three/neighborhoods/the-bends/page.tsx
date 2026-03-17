a3cd07be4d98baf88bb153bd07917514f1451e3f
Robert Rizzo <robtrizzo@gmail.com>
2026-03-17 10:23:49 -0400

    dominion faction

--- app/game/arc-three/neighborhoods/the-bends/page.tsx
+++ app/game/arc-three/neighborhoods/the-bends/page.tsx
@@ -20,7 +20,7 @@ export default async function Page() {
         dolorem aperiam asperiores quaerat? Quae at eaque dolores labore itaque.
       </TypographyP>
       <TypographyH2>Factions</TypographyH2>
-      <div className="my-4 spacing-y-2">
+      <div className="my-4 flex flex-col gap-2">
         <Card>
           <CardHeader>
             <TypographyH3>Southside Council</TypographyH3>
@@ -70,6 +70,60 @@ export default async function Page() {
               <li>
                 <b>Faction Clocks:</b> small council election [6], celebrate
                 Anidinian culture [8]
+              </li>
+            </TypographyUnorderedList>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <TypographyH3>Dominion</TypographyH3>
+            <span className="text-muted-foreground">
+              <b>Tier II</b>, <i>Backwards</i>
+            </span>
+          </CardHeader>
+          <CardContent>
+            <TypographyP>
+              The dregs of Audo&apos;s Gorgers, conquered by Hrodulfr and beaten
+              down by the <b>Delivery</b> riots. A strict Rathi hierarchy
+              teeming with thickblooded and thinblooded shifters starving for
+              their alpha&apos;s approval.
+            </TypographyP>
+            <TypographyUnorderedList>
+              <li>
+                <b>Turf:</b> the main thouroughfare of The Bends. Larger
+                residences and broader streets than elsewhere in Fabrication.
+              </li>
+              <li>
+                <b>Population</b>: ~2500.{" "}
+                <i>
+                  82% worker, 6% thickblooded shifter, 18% thinblooded shifter,
+                  18% enforcer
+                </i>
+              </li>
+              <li>
+                <b>NPCs:</b> <b>Hrodulfr</b>{" "}
+                <i>(leader, prideful, cruel, arrogant)</i>, <b>Thraus</b>{" "}
+                <i>(thickblooded wolf, cowardly, conniving)</i>, <b>Radegond</b>{" "}
+                <i>(thickblooded wolf, stalker, sadistic)</i>, <b>Bruno</b>{" "}
+                <i>(worker, adult, placating, smooth-talker)</i>
+              </li>
+              <li>
+                <b>Notable Assets:</b> large fighting force, Hrodulfr&apos;s
+                palace, a trove of armor and armaments
+              </li>
+              <li>
+                <b>Allies:</b> <i>none</i>
+              </li>
+              <li>
+                <b>Enemies:</b> Southside Council, Moore&apos;s Gang, Theta
+              </li>
+              <li>
+                <b>Situation:</b> after <b>Delivery</b>, Dominion&apos;s
+                fighters are wounded and morale is shaky.
+              </li>
+              <li>
+                <b>Faction Clocks:</b> tax surrounding factions [2], increase
+                tier [4], subjugate neighbor [8]
               </li>
             </TypographyUnorderedList>
           </CardContent>
