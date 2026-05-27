4875e2a745bf68039e931495325d7ec8b3decde2
Robert Rizzo <robtrizzo@gmail.com>
2026-05-27 08:52:24 -0400

    wip resource charts

--- app/game/arc-three/character-options/roles/page.tsx
+++ app/game/arc-three/character-options/roles/page.tsx
@@ -56,13 +56,49 @@ export default async function Page() {
       </TypographyP>
       <TypographyH2>Responsibilities</TypographyH2>
       <TypographyH3>Seeker</TypographyH3>
+      <TypographyP>
+        Explore the Steel Trap for better routes between sectors, access to new
+        ones, and establish supply lines for the resistance.
+      </TypographyP>
       <TypographyH3>Wright</TypographyH3>
+      <TypographyP>
+        Cannibalize whatever machines and scrap you can get your hands on to
+        invent and manufacture new gear for the resistance.
+      </TypographyP>
       <TypographyH3>Shade</TypographyH3>
+      <TypographyP>
+        Infiltrate your enemies and learn their secrets. Gain access to their
+        most heavily guarded structures.
+      </TypographyP>
       <TypographyH3>Lock</TypographyH3>
+      <TypographyP>
+        Protect the resistance from threats both without and within.
+      </TypographyP>
       <TypographyH3>Vault</TypographyH3>
+      <TypographyP>
+        Manage critical resources and establish supply chains to fuel the
+        resistance's efforts.
+      </TypographyP>
       <TypographyH3>Cipher</TypographyH3>
+      <TypographyP>
+        Peel back the layers of deceit heaped on you. Gain mastery of the
+        machines.
+      </TypographyP>
       <TypographyH3>Pact</TypographyH3>
+      <TypographyP>
+        Manage the many personalities and factions of the resistance. Keep them
+        from turning on each other.
+      </TypographyP>
       <TypographyH3>Scaffold</TypographyH3>
+      <TypographyP>
+        Turn this wretched factory into fortifications, community spaces, and
+        homes.
+      </TypographyP>
+      <TypographyH3>Auger</TypographyH3>
+      <TypographyP>
+        Create a fighting force capable of striking further into enemy
+        territory.
+      </TypographyP>
     </>
   );
 }
