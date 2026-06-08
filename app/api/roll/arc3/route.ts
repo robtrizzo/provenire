b4225c3058961777c3f028dd3edf8d8182f0dc7f
Robert Rizzo <robtrizzo@gmail.com>
2026-06-08 13:55:59 -0400

    enable all players to see all rolls

--- app/api/roll/arc3/route.ts
+++ app/api/roll/arc3/route.ts
@@ -3,7 +3,7 @@ import { getAllRollsArc3 } from "@/handlers/rolls";
 import { checkAuth } from "@/lib/auth";
 
 export async function GET(request: Request) {
-  const { session, error } = await checkAuth("player");
+  const { error } = await checkAuth("player");
   if (error) return error;
 
   const { searchParams } = new URL(request.url);
@@ -12,9 +12,6 @@ export async function GET(request: Request) {
 
   try {
     let rolls = await getAllRollsArc3(cursor, pageSize);
-    if (!session?.user?.role.includes("admin")) {
-      rolls = rolls.filter((roll) => roll.userid === session?.user?.id);
-    }
     return NextResponse.json(rolls);
   } catch (error) {
     console.error("Error getting rolls", error);
