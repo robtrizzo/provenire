1e633bb18f3907fd06704bbb2146f5c43dd20d59
Robert Rizzo <robtrizzo@gmail.com>
2026-06-16 11:30:24 -0400

    use container query for bonds column style

--- app/game/arc-three/play/(components)/sections/bonds-section.tsx
+++ app/game/arc-three/play/(components)/sections/bonds-section.tsx
@@ -6,12 +6,12 @@ import BondsDialog from "../dialogs/bonds-dialog";
 export default function BondsSection() {
   const { bonds } = useCharacterSheet();
   return (
-    <div>
+    <div className="@container">
       <TypographyH2 className="text-md mt-0 uppercase text-muted-foreground flex justify-between items-end">
         Bonds <BondsDialog />
       </TypographyH2>
       <div className="mt-1" />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+      <div className="grid grid-cols-1 @md:grid-cols-2 gap-1">
         {bonds.map((b, idx) => (
           <Action.Wrapper.Menu key={idx + b.name} action={b}>
             <Action.Wrapper.Rollable key={idx + b.name} action={b}>
