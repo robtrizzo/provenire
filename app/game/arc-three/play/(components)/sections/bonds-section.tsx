d2c7e420b16d3a61e06dd00aaadb209baa0cdbf5
Robert Rizzo <robtrizzo@gmail.com>
2026-06-16 11:23:12 -0400

    bonds section is a grid in larger layouts

--- app/game/arc-three/play/(components)/sections/bonds-section.tsx
+++ app/game/arc-three/play/(components)/sections/bonds-section.tsx
@@ -11,13 +11,15 @@ export default function BondsSection() {
         Bonds <BondsDialog />
       </TypographyH2>
       <div className="mt-1" />
-      {bonds.map((b, idx) => (
-        <Action.Wrapper.Menu key={idx + b.name} action={b}>
-          <Action.Wrapper.Rollable key={idx + b.name} action={b}>
-            <Action.HeaderContent.Simple action={b} />
-          </Action.Wrapper.Rollable>
-        </Action.Wrapper.Menu>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+        {bonds.map((b, idx) => (
+          <Action.Wrapper.Menu key={idx + b.name} action={b}>
+            <Action.Wrapper.Rollable key={idx + b.name} action={b}>
+              <Action.HeaderContent.Simple action={b} />
+            </Action.Wrapper.Rollable>
+          </Action.Wrapper.Menu>
+        ))}
+      </div>
       <Action.Wrapper.Grid className="p-0">
         <Action.HeaderContent.Unlock type="bond" className="p-2" />
       </Action.Wrapper.Grid>
